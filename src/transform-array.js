60f54bda416e914e51512b33907c4954b28bceea
julia <s98435@yandex.ru>
2021-03-13 10:06:32 +0300

    feat: +++transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(arr) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  if (Array.isArray(arr)) {
+    // --discard-next исключить слудующий элемент  массива
+    // --discard-prev исключить предыдущий элемент массива
+    // --double-next добавить следующий элемент
+    // --double-prev добавить преыдущий элемент
+    let transformArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '--discard-next') {
+        i++;
+        continue;
+      }
+      if(arr[i] === '--discard-prev' && transformArr.length > 0) {
+        transformArr.pop(transformArr.length -1);
+        continue;
+      }
+      if(arr[i] === '--double-next') {
+        transformArr.push(arr[i+1]);
+        continue;
+      }
+      if(arr[i] === '--double-prev') {
+        transformArr.push(arr[i-1]);
+      } else {
+        transformArr.push(arr[i]);
+        }
+
+    }
+
+    return transformArr;
+
+  } else {
+    return false;
+  }
+
 };
+
+
